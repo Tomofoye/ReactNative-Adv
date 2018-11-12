74d4ffbf50d5789174c0c4cc808bda717489b0ce
Tomofoye <tosinomofoye94@gmail.com>
2018-11-12 15:40:35 -0500

    tab-navigator depreciated fix

--- jobs/App.js
+++ jobs/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -8,7 +8,7 @@ import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
   render() {
-    const MainNavigator = TabNavigator({
+    const MainNavigator = createBottomTabNavigator({
       welcome: { screen: WelcomeScreen },
       auth: { screen: AuthScreen }
     });
@@ -25,7 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
